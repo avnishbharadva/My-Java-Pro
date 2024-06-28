a2ac24dca03ab2f830dd78bb608e39c6a78c4001
Avnish Bharadva <avnishbharadva@gmail.com>
2024-06-28 23:11:49 +0530

    instanceof operator

--- Monday.java
+++ Monday.java
@@ -1,19 +1,29 @@
 public class Monday {
     public static void main(String[] args) {
-        System.out.println("monday");
-        System.out.println(args[0]);
-        String str[] = {"anvish","anda"};
-        Tuesday.main(str);
-        Monday.main("avnish");
+        // System.out.println("monday");
+        // System.out.println(args[0]);
+        // String str[] = {"anvish","anda"};
+        // Tuesday.main(str);
+        // Monday.main("avnish");
+
+        // int a=1,b=2,c=5;
+        // a=b=c;
+        // a=b=c = 7;
+        // System.out.println("a : " + a + " b : " + b + " c : " + c);
+
+        // int i = 5;
+        System.out.println(15 << 1);
+        System.out.println(17 >> 1);
+        System.out.println(17 >>> 1);
     }   
 
-    public static void main(String args) {
-        System.out.println(args);
-    }
+    // public static void main(String args) {
+    //     System.out.println(args);
+    // }
 }
 
-class Tuesday{
-    public static void main(String[] args) {
-        System.out.println("tuesday");
-    }
-}
+// class Tuesday{
+//     public static void main(String[] args) {
+//         System.out.println("tuesday");
+//     }
+// }
