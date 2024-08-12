9902bbe30f20e33e5218ef23eb40a37c9ed39c73
Avnish Bharadva <avnishbharadva@gmail.com>
2024-08-13 01:01:51 +0530

    Multiple thread

--- ThreadEx.java
+++ ThreadEx.java
@@ -1,32 +1,30 @@
-class ThreadA extends Thread {
-    public void run() {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("A : " + i);
+public class ThreadEx extends Thread
+{
+    String task;
+
+    ThreadEx(String task)
+    {
+        this.task = task;
+    }
+    public void run()
+    {
+        for(int i=1;i<=12;i++)
+        {
+            System.out.println(this.task + " : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println(e.getMessage());
+            }
         }
     }
-}
 
-class ThreadB extends Thread {
-    public void run() {
-        for (int i = 1; i <= 10; i++) {
-            System.err.println("B : " + i);
-        }
-    }
-}
-
-public class ThreadEx {
-    public static void main(String args[]) {
-        try {
-            ThreadA oa = new ThreadA();
-            ThreadB ob = new ThreadB();
-            oa.start();
-            ob.start();
-            // oa.join(100000);
-            // ob.join(0);
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(e.getMessage());
-        }
-
+    public static void main(String args[])
+    {
+        ThreadEx oa = new ThreadEx("Kalgu");
+        oa.start();
+        ThreadEx ob = new ThreadEx("Radha krishan");
+        ob.start();
     }
 }
